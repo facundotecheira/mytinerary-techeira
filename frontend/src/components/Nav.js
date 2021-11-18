37218f854478c6eea7c714977e302d82694ca853
facundotecheira <facundotecheira00@gmail.com>
2021-11-18 00:11:59 -0300

    fix: Cambien el logo

--- frontend/src/components/Nav.js
+++ frontend/src/components/Nav.js
@@ -5,7 +5,7 @@ const Nav = () => {
       <nav class="navbar navbar-expand-lg navbar-light bg-light">
         <div class="container-fluid">
           <a class="navbar-brand" href="#s">
-          <img class="iconbrand" src="./assets/logo.jpg" alt="iconbrand"></img></a>
+          <img class="iconbrand" src="./assets/logo3.png" alt="iconbrand"></img></a>
           <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
           </button>
