e2e64800c913382864bc8545be03bebc9b0bc2b0
facundotecheira <facundotecheira00@gmail.com>
2021-12-31 00:44:10 -0300

    .

--- frontend/src/redux/actions/countrysAction.js
+++ frontend/src/redux/actions/countrysAction.js
@@ -3,7 +3,7 @@ const countrysAction = {
     getCountries: ()=>{
 
         return async(dispatch, getState) => {
-            let respuesta  = await axios.get('https://mytinerary-techeira.herokuapp.com/api/v0.1/countries')
+            let respuesta  = await axios.get('https://countriesnow.space/api/v0.1/countries')
             let info = respuesta.data.data
             dispatch({type:'GET_ALL_COUNTRIES',payload:info})
         }   
